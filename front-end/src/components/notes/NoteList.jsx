ca3ad9e2e9b3c571414166b9fbb12655f67e55d9
Emmanuel Tejuosho <emmanueltejuosho@gmail.com>
2021-01-12 09:03:55 -0500

    update noteList to not display empty notes

--- front-end/src/components/notes/NoteList.jsx
+++ front-end/src/components/notes/NoteList.jsx
@@ -9,7 +9,7 @@ function NoteList(props){
 
 
     useEffect(() => {
-        axios.get("/api/notes/")
+        axios.get("http://localhost:5000/api/notes/")
             .then(res => setNotes(res.data))
             .catch(err => console.log(err));
     }, []);
@@ -18,19 +18,23 @@ function NoteList(props){
     return (
         <div>
         <h2 style={{textAlign:'center'}}>
-        Notes
+        Your Notes
         <Link to="/notes/new" className="btn btn-primary float-right">Create Note</Link>
       </h2>
-      {notes.map((note) => {
+
+      {notes.map(note => {
           return(
+              (note.title && note.content) ? 
               <div className="note" key={note._id}>
-                <h1><Link to={"/notes/" + note._id}>{note.title}</Link></h1>
+                {<h1><Link to={"/notes/" + note._id}>{note.title}</Link></h1>}
                 <p>{note.content}</p>
                 {/* <Link to={`/notes/${note._id}/edit`} className="editButton"><EditIcon /></Link> 
                 {/* <button id="editButton" onClick={handleEdit}> <EditIcon /> </button> */}
                 {/* <button className="deleteButton" onClick={handleDelete}> <DeleteIcon /> </button> */}
                 {/* <Link to="/notes">Close</Link> */}
               </div>
+              : null
+
           );
       })}
         </div>
